7521e85856bc2c4b4f35dd0906851f215cb3270a
Vũ Đức Thiệp <vuducthiep011104@gmail.com>
2026-01-19 18:10:12 +0700

    Update CloudinaryConfig.java

--- StyleStore_BE/src/main/java/com/example/StyleStore/config/CloudinaryConfig.java
+++ StyleStore_BE/src/main/java/com/example/StyleStore/config/CloudinaryConfig.java
@@ -11,9 +11,6 @@ public class CloudinaryConfig {
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "dslt4mgvy",
-                "api_key", "825361189772776",
-                "api_secret", "MvpZwBT5znbLDrpmWcfRwzrLx_g",
-                "secure", true));
+                ));
     }
 }
